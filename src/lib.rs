17583ee43aec89fb68a4cd28283cc9e99520e5ce
Joe C <joe.caulfield@anza.xyz>
2024-03-05 16:54:11 -0600

    closer Anchor repro

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
+    msg,
     program::invoke,
+    program_error::ProgramError,
     pubkey::Pubkey,
     rent::Rent,
     sysvar::Sysvar,
@@ -14,29 +16,53 @@ solana_program::entrypoint!(process_instruction);
 // Adjust this constant to test behavior with different numbers of keys.
 pub const NUM_KEYS: usize = 14;
 
+struct MockSignerStruct<'a>(pub AccountInfo<'a>);
+
+impl<'a> MockSignerStruct<'a> {
+    fn try_from(account: &'a AccountInfo<'a>) -> Result<Self, ProgramError> {
+        if account.is_signer {
+            Ok(MockSignerStruct(account.clone()))
+        } else {
+            Err(ProgramError::MissingRequiredSignature)
+        }
+    }
+
+    fn try_accounts(accounts: &mut &'a [AccountInfo<'a>]) -> Result<Self, ProgramError> {
+        if accounts.is_empty() {
+            return Err(ProgramError::NotEnoughAccountKeys);
+        }
+        let account = &accounts[0];
+        *accounts = &accounts[1..];
+        MockSignerStruct::try_from(account)
+    }
+}
+
 fn process_instruction<'a>(
     program_id: &Pubkey,
-    accounts: &[AccountInfo],
+    accounts: &'a [AccountInfo<'a>],
     _input: &[u8],
 ) -> ProgramResult {
     let accounts_iter = &mut accounts.iter();
 
     let rent = <Rent as Sysvar>::get()?;
 
-    let signer = next_account_info(accounts_iter)?;
+    let payer = next_account_info(accounts_iter)?;
     let _system_program = next_account_info(accounts_iter)?;
 
+    let mut remaining_accounts: &mut &'a [AccountInfo<'a>] = &mut &accounts[2..];
+
     for _ in 0..NUM_KEYS {
-        let account = next_account_info(accounts_iter)?;
+        msg!("Remaining accounts: {:?}", remaining_accounts.len());
+        let signer = MockSignerStruct::try_accounts(&mut remaining_accounts)?;
         invoke(
             &solana_program::system_instruction::create_account(
-                &signer.key,
-                &account.key,
+                &payer.key,
+                &signer.0.key,
                 rent.minimum_balance(8),
                 8,
                 program_id,
             ),
-            &[account.clone(), signer.clone()],
+            &[signer.0.clone(), payer.clone()],
         )?;
     }
 
